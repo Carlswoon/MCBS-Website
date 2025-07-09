70c07e55a8e0a6851e13b2f945d83de348ee4163
Carlswoon <carlson280306@gmail.com>
2025-07-09 19:25:15 +1000

    made the countdown banner conditional for mobile devices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import CountdownBanner from '../components/CountdownSection';
 import Carousel3D from '../components/MahjongTileCarousel';
+import { useIsMobile } from '../hooks/useIsMobile';
 
 export default function Home() {
+  const isMobile = useIsMobile();
+
   return (
     <>
       <Carousel3D />
-      <CountdownBanner />
+      {!isMobile && <CountdownBanner />}
     </>
   );
 }
